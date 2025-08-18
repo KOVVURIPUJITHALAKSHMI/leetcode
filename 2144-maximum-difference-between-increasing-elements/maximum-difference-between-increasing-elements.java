f288593407ad53c2080901f7d0d901858bbac390
KOVVURI PUJITHA LAKSHMI <119839999+KOVVURIPUJITHALAKSHMI@users.noreply.github.com>
2025-08-18 17:52:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (45.00%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -1,14 +1,25 @@
 class Solution {
     public int maximumDifference(int[] nums) {
-        int maxSum=0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[j]-nums[i]>maxSum){
-                    maxSum=nums[j]-nums[i];
-                }
+        // int maxSum=0;
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[j]-nums[i]>maxSum){
+        //             maxSum=nums[j]-nums[i];
+        //         }
+        //     }
+        // }
+        // return maxSum==0 ? -1:maxSum;
+        int minValue=nums[0];
+        int maxSum=-1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>minValue){
+                maxSum=Math.max(maxSum,nums[i]-minValue);
+            }
+            else{
+                minValue=nums[i];
             }
         }
-        return maxSum==0 ? -1:maxSum;
+        return maxSum;
         
     }
 }
